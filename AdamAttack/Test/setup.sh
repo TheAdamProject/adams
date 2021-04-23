e144c8a47f254b4adcd55198e201dc9222fb116d
pasquini-dario <44810501+pasquini-dario@users.noreply.github.com>
2021-04-23 17:51:46 +0200

    Update setup.sh

--- AdamAttack/Test/setup.sh
+++ AdamAttack/Test/setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 source ./banner.sh
-NAME_ZIP="passwords.zip"
 
 mkdir -p passwords
 cd passwords
-wget --no-check-certificate $PASSWORD_URL$NAME_ZIP -O $NAME_ZIP
-unzip -o $NAME_ZIP
-rm $NAME_ZIP
+wget --no-check-certificate "https://kelvin.iac.rm.cnr.it/passwords.zip"
+unzip passwords.zip
+rm passwords.zip
 
 cd ..
 
